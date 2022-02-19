5a15e8b4aed9ba9aefb41f50e986646edf25996c
Josh <pimentelsj@gmail.com>
2022-02-19 19:21:12 -0300

    -Replace port Env variable -set URL fetch function -Add start script for Heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -104,6 +105,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is runing up at port 3000.')
+app.listen(port, () => {
+    console.log(`Server is runing up at port ${port}.`)
 })
